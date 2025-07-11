de4a911b2e5d6b5ebbef98cbbb5eeef18b0b095d
ryhxase <nrayha55@gmail.com>
2025-07-12 00:35:51 +0700

    test

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'bayar_kampus'
+    user: 'kodehanc_kodehan',
+    password: '2p0]7{O)V6zK',
+    database: 'kodehanc_kodehan'
 });
 
 const db = pool.promise();
