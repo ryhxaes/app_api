dd20557875733cd0e1251a16e0bc814480af7499
ryhxase <nrayha55@gmail.com>
2025-07-12 01:05:13 +0700

    test

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: '127.0.0.1',
+    host: '103.125.180.35',
     user: 'kodehanc_kodehan',
     password: '2p0]7{O)V6zK',
     database: 'kodehanc_kodehan'
